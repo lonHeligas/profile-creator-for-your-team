351a1da29aaed3e0bf7bea4cb94ea3032cf704c3
Lon Heligas <lon.heligas@gmail.com>
2023-01-27 19:36:59 -0600

    started Engineer test development

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,14 @@
 const Engineer = require("../lib/Engineer.js");
+
+describe('Engineer() class', () => {
+  it.todo('should update the employee with the engineer sub-class', () => {
+    expect(new Engineer()).not.toBeUndefined()
+  })
+  it('should accept input for the github username and return it', () => {
+    const engineer = new Engineer('stackerPentacost');
+    expect(engineer.getgithub()).toBe('stackerPentacost')    
+  })
+  it.todo('should accept the role as engineer', () => {
+    const engineer = new Engineer('CORBIN DALLAS')
+  })
+});
