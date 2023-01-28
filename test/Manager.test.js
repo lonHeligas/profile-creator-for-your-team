6fabda87abd4f782d3a7714b74914b809e27e75e
Lon Heligas <lon.heligas@gmail.com>
2023-01-27 22:30:35 -0600

    added manager testing

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,1 +1,14 @@
 const Manager = require("../lib/Manager.js");
+
+describe('Manager() class', () => {
+  it('should update the employee with the manager sub-class', () =>{expect(new Manager()).not.toBeUndefined();
+  });
+  it('should accpet input for the office number and return it', () => {
+    const manager = new Manager('WILLIAM ADAMA', 'theid', 'theemail', 'bsg75');
+    expect(manager.getOfficeNumber()).toBe('bsg75')
+  });
+  it('should accept the role as manager', () => {
+    const manager = new Manager('WILLIAM ADAMA');
+    expect(manager.getRole()).toEqual('manager');
+  });
+})
