b56a54bd1e14f694c6a2b67bbf31b79586e33ff4
Lon Heligas <lon.heligas@gmail.com>
2023-01-28 14:31:20 -0600

    adding main questions via inquiry

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+let teamName = "";
+
+
+/*
+dummy data
+*/
+
+function enterManagerData(){
+
+}
+
+
+
+function whichRole(){
+
+}
+
+
+
+function start(){
+  // console.log(`testing`)
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Welcome to your Team Profile Builder! What's the name of your team?",
+      name: "teamName", 
+      // validate: (response) => {
+      //   if (response === ''){
+      //     return 'You still need to provide a name for your team.'
+      //   }
+      //   return true
+      // }     
+    },    
+  ])
+
+  console.log(response.name);
+  
+};
+
+
+
+
+start();
---
+++ todo.js
@@ -0,0 +1,25 @@
+// todo Start
+// todo Start the questions
+// todo      ask information for the manger
+// todo         set manager status
+// todo      branch to engineer employee or intern employee
+
+// todo      branch to engineer
+// todo        ask questions
+// todo        add to engineer array
+// todo        ask if the user wants to add another employee 
+// todo        return if yes
+
+// todo      branch to intern
+// todo        ask questions
+// todo        add to intern array
+// todo        ask if the user wants to add anoter employee 
+// todo        return if yes
+
+// todo      if no, ask for filename
+// todo      build out html in this order:
+// todo           manager, engineers, interns
+// todo      make sure the css is set for this as well (bootstrap?)
+
+
+// todo      <end of line>
